a46e247b8aa99322b35b9ab9741168a7a7411b0e
Andrii Stepaniuk <Andrii.Stepaniuk@firstgroup.com>
2025-01-20 20:51:28 +0200

    main code

--- string_operations.test.js
+++
@@ -1,9 +0,0 @@
-function isPalindrome(str) {
-    const cleaned = str.toLowerCase().split(' ').join('');
-    return cleaned === cleaned.split('').reverse().join('');
-}
-
-function isAnagram(str1, str2) {
-    const format = str => str.toLowerCase().split(' ').join('').split('').sort().join('');
-    return format(str1) === format(str2);
-}
